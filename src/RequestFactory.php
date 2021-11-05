8e535f910ca215fbc762c7a6869a021cd402fd2a
Anatoly Nekhay <afenric@gmail.com>
2021-11-05 12:15:42 +0500

    new method RequestFactory::createJsonRequest

--- src/RequestFactory.php
+++ src/RequestFactory.php
@@ -16,6 +16,20 @@ namespace Sunrise\Http\Message;
  */
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+use InvalidArgumentException;
+
+/**
+ * Import functions
+ */
+use function json_encode;
+use function json_last_error;
+use function json_last_error_msg;
+
+/**
+ * Import constants
+ */
+use const JSON_ERROR_NONE;
 
 /**
  * HTTP Request Message Factory
@@ -31,5 +45,41 @@ class RequestFactory implements RequestFactoryInterface
     public function createRequest(string $method, $uri) : RequestInterface
     {
         return new Request($method, $uri);
+    }
+
+    /**
+     * Creates JSON request
+     *
+     * @param string $method
+     * @param string|UriInterface|null $uri
+     * @param mixed $data
+     * @param int $options
+     * @param int $depth
+     *
+     * @return RequestInterface
+     *
+     * @throws InvalidArgumentException
+     *         If the data cannot be encoded.
+     */
+    public function createJsonRequest(
+        string $method,
+        $uri,
+        $data,
+        int $options = 0,
+        int $depth = 512
+    ) : RequestInterface {
+        json_encode(''); // reset previous error...
+        $content = json_encode($data, $options, $depth);
+        if (JSON_ERROR_NONE <> json_last_error()) {
+            throw new InvalidArgumentException(json_last_error_msg());
+        }
+
+        $request = new Request($method, $uri, [
+            'Content-Type' => 'application/json; charset=UTF-8',
+        ]);
+
+        $request->getBody()->write($content);
+
+        return $request;
     }
 }
